ce39f7495e2c9d4b2f89e651424fb2ce35d2273f
Allison Snipes <ajsnipes@usfca.edu>
2019-06-20 06:30:41 -0700

    setting up files

---
+++ app.js
@@ -0,0 +1,8 @@
+// *********************************************************************************
+// Packages required to make project run
+// *********************************************************************************
+
+// dependencies
+const express = require('express')
+const app = express()
+const path = require('path')
