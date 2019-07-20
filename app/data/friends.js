3aaa35afdfe947e3bd7aa172f1572966fd3189e9
Allison Snipes <ajsnipes@usfca.edu>
2019-07-20 12:31:20 -0700

    rdo

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,131 +1,49 @@
-const friends = [{
-  'name': 'Jana',
-  'photo': 'https://images.unsplash.com/photo-1540939316454-33b6922f0aac?ixlib=rb-1.2.1&w=1000&q=80',
-  'scores': [
-    5,
-    5,
-    5,
-    5,
-    5,
-    5,
-    5,
-    5,
-    5,
-    5
-  ]
-},
-{
-  'name': 'Bob',
-  'photo': 'http://pm1.narvii.com/5866/76b201e12343e860570c62f4067307e04ed545fe_00.jpg',
-  'scores': [
-    1,
-    1,
-    1,
-    1,
-    1,
-    1,
-    1,
-    1,
-    1,
-    1
-  ]
-},
-{
-  'name': 'Emily',
-  'photo': 'https://qph.fs.quoracdn.net/main-raw-765019248-pfnsqnoooqfrbxonlecqikeknuxrqsxy.jpeg',
-  'scores': [
-    5,
-    5,
-    5,
-    5,
-    5,
-    1,
-    1,
-    1,
-    1,
-    1
-  ]
-},
-{
-  'name': 'Olivia',
-  'photo': 'https://encrypted-tbn0.gstatic.com/imagesq=tbn:ANd9GcSldRAi9EORRCn2NEd7g1ha46TPMpuA-TJ7IJVWiznVRVl03XD5nw',
-  'scores': [
-    5,
-    5,
-    2,
-    1,
-    4,
-    1,
-    3,
-    3,
-    5,
-    5
-  ]
-},
-{
-  'name': 'Sadie',
-  'photo': 'https://encrypted-tbn0.gstatic.com/imagesq=tbn:ANd9GcQ18Qm4mQ9moiaJMr469q-OJIC0vcUC0dTA0F4__45RuKE3u8hz',
-  'scores': [
-    4,
-    5,
-    2,
-    2,
-    1,
-    4,
-    2,
-    1,
-    4,
-    5
-  ]
-},
-{
-  'name': 'Jocelyn',
-  'photo': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzhQkBMWT8sIsrsPi33jlVWnFJ3WxdWqpkuvHDXee6h4ELWwIEtQ',
-  'scores': [
-    2,
-    2,
-    2,
-    2,
-    4,
-    1,
-    5,
-    5,
-    5,
-    1
-  ]
-},
-{
-  'name': 'Yoko',
-  'photo': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2sZbJZl4rXzSuCYKvQ6Wxdf5BeuI-Kx98zuO2-6w4jN3kpeZjiQ',
-  'scores': [
-    5,
-    3,
-    1,
-    2,
-    2,
-    4,
-    1,
-    5,
-    2,
-    1
-  ]
-},
-{
-  'name': 'Lia',
-  'photo': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVWNBeLFNwkQH2kCZ8TLHwHOuynzwq60XGpKyKUr-3mLu3CiJy',
-  'scores': [
-    3,
-    1,
-    1,
-    5,
-    2,
-    1,
-    2,
-    5,
-    4,
-    5
-  ]
-}
-]
+const friends = [
+	{
+		name: 'Jana',
+		photo: 'https://images.unsplash.com/photo-1540939316454-33b6922f0aac?ixlib=rb-1.2.1&w=1000&q=80',
+		scores: [ 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 ]
+	},
+	{
+		name: 'Bob',
+		photo: 'https://timedotcom.files.wordpress.com/2017/12/terry-crews-person-of-year-2017-time-magazine-2.jpg',
+		scores: [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ]
+	},
+	{
+		name: 'Emily',
+		photo: 'https://qph.fs.quoracdn.net/main-raw-765019248-pfnsqnoooqfrbxonlecqikeknuxrqsxy.jpeg',
+		scores: [ 5, 5, 5, 5, 5, 1, 1, 1, 1, 1 ]
+	},
+	{
+		name: 'Olivia',
+		photo:
+			'https://encrypted-tbn0.gstatic.com/imagesq=tbn:ANd9GcSldRAi9EORRCn2NEd7g1ha46TPMpuA-TJ7IJVWiznVRVl03XD5nw',
+		scores: [ 5, 5, 2, 1, 4, 1, 3, 3, 5, 5 ]
+	},
+	{
+		name: 'Sadie',
+		photo:
+			'https://encrypted-tbn0.gstatic.com/imagesq=tbn:ANd9GcQ18Qm4mQ9moiaJMr469q-OJIC0vcUC0dTA0F4__45RuKE3u8hz',
+		scores: [ 4, 5, 2, 2, 1, 4, 2, 1, 4, 5 ]
+	},
+	{
+		name: 'Jocelyn',
+		photo:
+			'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzhQkBMWT8sIsrsPi33jlVWnFJ3WxdWqpkuvHDXee6h4ELWwIEtQ',
+		scores: [ 2, 2, 2, 2, 4, 1, 5, 5, 5, 1 ]
+	},
+	{
+		name: 'Yoko',
+		photo:
+			'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2sZbJZl4rXzSuCYKvQ6Wxdf5BeuI-Kx98zuO2-6w4jN3kpeZjiQ',
+		scores: [ 5, 3, 1, 2, 2, 4, 1, 5, 2, 1 ]
+	},
+	{
+		name: 'Lia',
+		photo:
+			'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVWNBeLFNwkQH2kCZ8TLHwHOuynzwq60XGpKyKUr-3mLu3CiJy',
+		scores: [ 3, 1, 1, 5, 2, 1, 2, 5, 4, 5 ]
+	}
+];
 
-module.exports = friends
+module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,37 +9,37 @@ module.exports = function (app) {
   // Get method route
   app.get('/api/friends', function (req, res) {
     res.json(neededData)
-  })
+	})
 
-  // post method route
-  app.post('/api/friends', function (req, res) {
-    let userScore = req.body.scores // pull the user score from the form post
-    const scoresArr = [] // set up an empty array to hold the score
-    let bestMatch = 0 // set initial best match to 0
+	// post method route
+	app.post('/api/friends', function (req, res) {
+    console.log(req.body)
 
-    // loop through friendData json file to get users score and save it to a variable
-    for (let i = 0; i < neededData.length; i++) {
+		let userScore = req.body.scores // pull the user score from the form post
+		const scoresArr = [] // set up an empty array to hold the score
+		let bestMatch = 0 // set initial best match to 0
+		// loop through friendData json file to get users score and save it to a variable
+		for (let i = 0; i < neededData.length; i++) {
       var scoreDiff = 0
-      for (let j = 0; j < userScore.length; j++) {
-        scoreDiff += (Math.abs(parseInt(neededData[i].scores[j]) - parseInt(userScore[j])))
-      }
+			for (let j = 0; j < userScore.length; j++) {
+        scoreDiff += Math.abs(parseInt(neededData[i].scores[j]) - parseInt(userScore[j]))
+			}
       scoresArr.push(scoreDiff) // use the push method to push the difference score
-    }
-
-    //loop through scoreArr to find the best match
+		}
+    // loop through scoreArr to find the best match
     for (let i = 0; i < scoresArr.length; i++) {
       if (scoresArr[i] <= scoresArr[bestMatch]) {
         bestMatch = i
-      }
+			}
     }
-
-    //return the best match to user
+    // return the best match to user
     let perfectMatch = neededData[bestMatch]
-    res.json(perfectMatch)
-    neededData.push(req.body)
-  })
+		res.json(perfectMatch)
+		neededData.push(req.body)
+	})
 
-  // Delete an example by id
-  app.delete('/api/friends/:id', function (req, res) {
+	// Delete an example by id
+	app.delete('/api/friends/:id', function (req, res) {
     res.json(surveyData)
-  })
+	})
+};
